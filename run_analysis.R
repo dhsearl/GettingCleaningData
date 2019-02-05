8758adfc26bd4f132eeca67fddf7deeecd9a4c49
David Searl <dhsearl@gmail.com>
2019-02-04 21:00:45 -0600

    Doesn't need data folder

--- run_analysis.R
+++ run_analysis.R
@@ -6,29 +6,26 @@
 ## 3. Uses descriptive activity names to name the activities in the data set
 ## 4. Appropriately labels the data set with descriptive variable names.
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-library(dplyr)
-library(readr)
-library(plyr)
 
 ## Load both Train and Test into tibbles
-test <- read_delim("./data/UCI HAR Dataset/test/X_test.txt", col_names = F, delim = " ", col_types = list(.default = col_number()))
-test_activities <- read_delim("./data/UCI HAR Dataset/test/y_test.txt", col_names = F, delim = " ")
-test_subjects <- read_delim("./data/UCI HAR Dataset/test/subject_test.txt", col_names = F, delim = " ")
+test <- read_delim("./UCI HAR Dataset/test/X_test.txt", col_names = F, delim = " ", col_types = list(.default = col_number()))
+test_activities <- read_delim("./UCI HAR Dataset/test/y_test.txt", col_names = F, delim = " ")
+test_subjects <- read_delim("./UCI HAR Dataset/test/subject_test.txt", col_names = F, delim = " ")
 test <- cbind(test_subjects, test_activities,test)
 
-train <- read_delim("./data/UCI HAR Dataset/train/X_train.txt", col_names = F, delim = " ", col_types = list(.default = col_number()))
-train_activities <- read_delim("./data/UCI HAR Dataset/train/y_train.txt", col_names = F, delim = " ")
-train_subjects <- read_delim("./data/UCI HAR Dataset/train/subject_train.txt", col_names = F, delim = " ")
+train <- read_delim("./UCI HAR Dataset/train/X_train.txt", col_names = F, delim = " ", col_types = list(.default = col_number()))
+train_activities <- read_delim("./UCI HAR Dataset/train/y_train.txt", col_names = F, delim = " ")
+train_subjects <- read_delim("./UCI HAR Dataset/train/subject_train.txt", col_names = F, delim = " ")
 train <- cbind(train_subjects, train_activities,train)
 
-## 1. Lets rbind these two into a full set.
+## 1. Lets rbind the full set.
 full_set <- rbind(test,train)
 
-## Lets clean up our environment by removing the old data tables
+## Lets clean up our environment
 rm(list = c("test","test_activities","test_subjects","train","train_activities","train_subjects"))
 
 ## 3. Lets add in headers. First pull from features, then add on subject and activity
-headers <- read_delim("./data/UCI HAR Dataset/features.txt", col_names=F, delim=" ")[,2]
+headers <- read_delim("./UCI HAR Dataset/features.txt", col_names=F, delim=" ")[,2]
 colnames(full_set) <- c("subject", "activity", headers[[1]])
 rm(headers)
 
@@ -42,14 +39,14 @@ full_set$activity <- as.factor(full_set$activity)
 smaller_set <- full_set[ , grepl("^subject|^activity|std|mean",colnames(full_set)) ]
 
 ## 4. load activity labels, then using mapvalues, replace the activity numbers with activity names
-activity_labels <- read_delim("./data/UCI HAR Dataset/activity_labels.txt", col_names = F, delim=" ")
+activity_labels <- read_delim("./UCI HAR Dataset/activity_labels.txt", col_names = F, delim=" ")
 smaller_set$activity <- mapvalues(smaller_set$activity, activity_labels$X1, activity_labels$X2)
 rm(activity_labels)
 
 ## 5. Create second, independent tidy data set with the average of each variable for each activity and each subject
 final_tidy_tibble <- smaller_set %>% group_by(activity, subject) %>% summarise_all(mean)
 
-write.table(final_tidy_tibble, "./data/UCI HAR Dataset/final_tidy_table.txt", row.names = F)
+write.table(final_tidy_tibble, "./UCI HAR Dataset/final_tidy_table.txt", row.names = F)
 ## Thanks!
 
 ##Not Needed since features.txt is as descriptive and clear as needed.
